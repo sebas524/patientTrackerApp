a3e50611b2f187798c9e6ef7ef4739666ef2c189
sebas524 <104307242+sebas524@users.noreply.github.com>
2023-08-29 20:35:14 +0200

    update title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,8 @@ const Title = () => {
   return (
     <div>
       <h1 className="mx-auto mb-2 font-black text-4xl text-center ">
-        Patient Check-In <span className="text-indigo-600 block ">Tracker</span>
+        Patient Check-In{" "}
+        <span className="text-indigo-600 block ">Tracker App</span>
       </h1>
       <p className="text-center my-6 ">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel autem id
